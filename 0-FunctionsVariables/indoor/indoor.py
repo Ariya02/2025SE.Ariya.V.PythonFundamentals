8d2a13dead1df9a82d56fc66f7d920baa17c3429
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-02-06 04:12:14 +0000

    Upload completed indoor.py

---
+++ 0-FunctionsVariables/indoor/indoor.py
@@ -0,0 +1,7 @@
+print("Hello world")
+name = input("What's your name? ").strip().capitalize()
+
+print(f"Hello, {name}")
+
+hobby = input("What's your favourite hobby? ").strip().casefold()
+print(f"I like {hobby} too, {name}!")
