ab3c2565174c8f327b686442113e6d95480d80e4
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-02-12 10:32:28 +0000

    Upload completed meal.py

---
+++ 1-Conditionals/meal/meal.py
@@ -0,0 +1,10 @@
+time = input("what is the time? ")
+
+if time.startswith("7"):
+    print("Breakfast time")
+elif time.startswith("12"):
+    print("Lunch time")
+elif time.startswith("18"):
+    print("Dinner time")
+else:
+    print("")
