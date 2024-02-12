d726e382bedaf6dc0cf60cd8a7f930def7d89914
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2024-02-12 01:51:22 +0000

    intepreter.py work in progress

---
+++ 1-Conditionals/interpreter/interpreter.py
@@ -0,0 +1,19 @@
+def main():
+    expression = expression_to_float(input("Write an arithmetic expression "))
+
+
+def expression_to_float(a, b, c):
+    a, b, c = expression.split(" ")
+    print(a, b, c)
+    return float
+
+
+# def equation():
+#    if b == ("+"):
+#        print((a) "+" (b))
+#    elif b == ("-"):
+#        print("-")
+#    elif b == ("*"):
+#        print("*")
+#    elif b == ("/"):
+#        print("%")
