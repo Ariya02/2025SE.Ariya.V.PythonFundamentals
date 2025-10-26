7f1a05ea494ad0ee3a677c29293ce4f57a4b838a
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2025-10-26 05:44:21 +0000

    Upload completed deep.py

--- 1-Conditionals/deep/deep.py
+++ 1-Conditionals/deep/deep.py
@@ -1,12 +1,13 @@
-answer = str(
-    input("What is the answer to the Great Question of Life, Universe and Everything? ")
+question = input(
+    "Provide an answer for the Great Question of Life, the Universe and Everything: "
 )
+answer = question.lower()
 
 if answer == "42":
     print("Yes")
-elif answer == "forty two":
-    print("Yes")
 elif answer == "forty-two":
+    print("Yes")
+elif answer == "forty two":
     print("Yes")
 else:
     print("No")
