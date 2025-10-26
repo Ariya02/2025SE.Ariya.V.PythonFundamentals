d5fbeaadd2709737c9c060856f25f01587bed4be
Ariya02 <124547357+Ariya02@users.noreply.github.com>
2025-10-26 06:29:04 +0000

    Upload completed extensions.py

--- 1-Conditionals/extensions/extensions.py
+++ 1-Conditionals/extensions/extensions.py
@@ -1,18 +1,18 @@
-suffix = input("Enter a file name suffix ")
+extension = (input("Enter filename: ")).lower()
 
-if suffix.endswith(".gif"):
+if ".gif" in extension:
     print("image/gif")
-elif suffix.endswith(".jpg"):
-    print("image/jpg")
-elif suffix.endswith(".jpeg"):
+elif ".jpg" in extension:
     print("image/jpeg")
-elif suffix.endswith(".png"):
+elif ".jpeg" in extension:
+    print("image/jpeg")
+elif ".png" in extension:
     print("image/png")
-elif suffix.endswith(".pdf"):
+elif ".pdf" in extension:
     print("application/pdf")
-elif suffix.endswith(".txt"):
-    print("image/txt")
-elif suffix.endswith(".zip"):
-    print("file/zip")
+elif ".txt" in extension:
+    print("text/plain")
+elif ".zip" in extension:
+    print("ZIP archive")
 else:
-    print("application/octet-stream")
+    print("application / octet - stream")
